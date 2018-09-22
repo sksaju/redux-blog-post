69e771c7bb6e3bb3fccc4ba7059ea564e0578580
unknown <sksaaju@gmail.com>
2018-09-22 00:51:54 -0700

    Add index route and component

---
+++ src/components/post_index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class PostIndex extends Component {
+  render() {
+    return (
+      <div>Post index</div>
+    );
+  }
+}
+
+export default PostIndex;
--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-import App from './components/app';
 import reducers from './reducers';
+import PostIndex from './components/post_index';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <BrowserRouter>
+            <Route path="/" component={PostIndex} />
+        </BrowserRouter>
+    </Provider>
+    , document.querySelector('.container'));
