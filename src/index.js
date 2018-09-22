07883bc1c2952d239360e8189fe6bb26203d37d3
unknown <sksaaju@gmail.com>
2018-09-22 01:29:35 -0700

    Add react-promise and fetchPosts action

--- src/index.js
+++ src/index.js
@@ -3,16 +3,19 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom';
+import promise from 'redux-promise';
 
 import reducers from './reducers';
 import PostIndex from './components/post_index';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
         <BrowserRouter>
-            <Route path="/" component={PostIndex} />
+            <div>
+                <Route path="/" component={PostIndex} />
+            </div>
         </BrowserRouter>
     </Provider>
     , document.querySelector('.container'));
