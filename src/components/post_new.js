766dcc6e10e4e0c31427b1bd5c6fb2191f3f3923
unknown <sksaaju@gmail.com>
2018-09-23 02:17:26 -0700

    Add new route and component post new

---
+++ src/components/post_new.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PostNew extends Component {
+    render() {
+        return (
+            <div>
+                Post New
+            </div>
+        );
+    };
+}
+
+export default PostNew;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import promise from 'redux-promise';
 
 import reducers from './reducers';
 import PostIndex from './components/post_index';
+import PostNew from './components/post_new';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
@@ -14,7 +15,10 @@ ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
         <BrowserRouter>
             <div>
-                <Route path="/" component={PostIndex} />
+                <Switch>
+                    <Route path="/posts/new" component={PostNew} />
+                    <Route path="/" component={PostIndex} />
+                </Switch>
             </div>
         </BrowserRouter>
     </Provider>
