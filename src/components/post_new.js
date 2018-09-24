7db5b32d5c24a0632c0d47f398e1fffb60bd408c
unknown <sksaaju@gmail.com>
2018-09-24 03:40:04 -0700

    More on Navigation

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
 
 class PostNew extends Component {
     renderField(field) {
@@ -44,6 +45,7 @@ class PostNew extends Component {
                     component={this.renderField}
                 />
                 <button type="submit" className="btn btn-success">Submit</button>
+                <Link to="/" className="btn btn-danger">Cancel</Link>
             </form>
         );
     };
