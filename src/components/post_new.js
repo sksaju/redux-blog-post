8f537f4eb1ceb8f893de4c70f1ebf0c56494a310
unknown <sksaaju@gmail.com>
2018-09-24 03:22:24 -0700

    Conditional Styling

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -3,15 +3,19 @@ import { Field, reduxForm } from 'redux-form';
 
 class PostNew extends Component {
     renderField(field) {
+        const {meta: {touched, error}} = field;
+        const className = `form-group ${(touched && error) ? 'has-danger' : ''}`;
         return (
-            <div className="form-group">
+            <div className={className}>
                 <label>{field.label}</label>
                 <input 
                     type="text"
                     className="form-control"
                     {...field.input}
                 />
-                { (field.meta.touched) ? field.meta.error : '' }
+                <div className="text-help">
+                    { (touched) ? error : '' }
+                </div>
             </div>
         );
     }
