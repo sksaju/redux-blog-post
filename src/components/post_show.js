eeb7326bf228b44160cdc077725f0464daeec1fa
unknown <sksaaju@gmail.com>
2018-09-25 22:24:41 -0700

    The Posts Show Component

---
+++ src/components/post_show.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PostShow extends Component {
+    render() {
+        return (
+            <div>
+                post show
+            </div>
+        );
+    };
+}
+
+export default PostShow;
--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import promise from 'redux-promise';
 import reducers from './reducers';
 import PostIndex from './components/post_index';
 import PostNew from './components/post_new';
+import PostShow from './components/post_show';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
@@ -17,6 +18,7 @@ ReactDOM.render(
             <div>
                 <Switch>
                     <Route path="/posts/new" component={PostNew} />
+                    <Route path="/posts/:id" component={PostShow} />
                     <Route path="/" component={PostIndex} />
                 </Switch>
             </div>
