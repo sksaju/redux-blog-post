810f213add7689b183a389f80705bb706d6bd2fc
unknown <sksaaju@gmail.com>
2018-09-26 01:39:32 -0700

    Data Dependencies

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 export const FETCH_POSTS = 'fetch_posts';
+export const FETCH_POST = 'fetch_post';
 export const CREATE_POSTS = 'create_posts';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
@@ -9,6 +10,14 @@ export function fetchPosts() {
     const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
     return {
         type: FETCH_POSTS,
+        payload: request
+    }
+}
+
+export function fetchPost(id) {
+    const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
+    return {
+        type: FETCH_POST,
         payload: request
     }
 }
--- src/components/post_show.js
+++ src/components/post_show.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions';
 
 class PostShow extends Component {
+    componentDidMount() {
+        const { id } = this.props.match.params;
+        this.props.fetchPost(id);
+    }
     render() {
+        const { post } = this.props;
+        if(!post) {
+            return <div>Loading...</div>;
+        }
         return (
             <div>
-                post show
+                <h3>{post.title}</h3>
+                <h6>{post.categories}</h6>
+                <p>{post.content}</p>
             </div>
         );
     };
 }
 
-export default PostShow;
+function mapStateToProps({ posts }, ownProps) {
+    return {post: posts[ownProps.match.params.id]};
+}
+
+export default connect(mapStateToProps, { fetchPost }) (PostShow);
